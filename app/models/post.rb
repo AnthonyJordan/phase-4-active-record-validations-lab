2cb168ba036c25e9fba59d10aed202c8cfd37a28
Anthony <Kain235@gmail.com>
2022-06-28 12:59:54 -0400

    commit tree 59ee519c03d259d5806ca6e864622a445c06a1fc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion:{in: %w(Fiction Non-Fiction)}
+    validate :is_title_clickbait
+
+    def is_title_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Clickbait not allowed")
+        end
+    end
 end
